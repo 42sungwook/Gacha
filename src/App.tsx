b2c95b0bfc4ebf00fec4b9fe4c40c5b39baaa9a4
Sungwook Kim <silenc3@naver.com>
2025-08-16 16:12:29 +0900

    test: mesh example

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,51 @@
-import './App.css';
+import './App.css'
+
+import * as THREE from 'three'
+import { useRef, useState } from 'react'
+import { Canvas, useFrame, type ThreeElements } from '@react-three/fiber'
+
+function Box(props: ThreeElements['mesh']) {
+  const meshRef = useRef<THREE.Mesh>(null!)
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
+  useFrame((_, delta) => (meshRef.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={meshRef}
+      scale={active ? 1.5 : 1}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : '#2f74c0'} />
+    </mesh>
+  )
+}
 
 const App = () => {
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight
+          position={[-10, -10, -10]}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
