869a41f0c4d42b29212eb3aab7db81c37e8d0211
Sungwook Kim <silenc3@naver.com>
2025-08-20 13:44:42 +0900

    feat: add vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import { Analytics } from '@vercel/analytics/react'
 
-const rootEl = document.getElementById('root');
+const rootEl = document.getElementById('root')
 if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
+  const root = ReactDOM.createRoot(rootEl)
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>,
-  );
+      <Analytics />
+    </React.StrictMode>
+  )
 }
